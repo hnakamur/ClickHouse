aa805e6c737bbc1bbda1ca05f8b78e28e9d82959
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2017-08-18 20:03:36 +0300

    updated GatherUtils

--- dbms/src/Common/TypeList.h
+++ dbms/src/Common/TypeList.h
@@ -18,17 +18,17 @@ struct TypeList<>
 {
 };
 
-template <template <typename ...> typename Class, template <typename ...> typename List, typename ... Types>
+template <template <typename ...> typename Class, typename List, typename ... Types>
 struct ApplyTypeListForClass;
 
-template <template <typename ...> typename Class, template <typename Type, typename ...> typename List, typename ... Types>
+template <template <typename ...> typename Class, typename List, typename ... Types>
 struct ApplyTypeListForClass<Class, List, Types ...>
 {
-    using Type = typename ApplyTypeListForClass<Class, typename List::Tail, Type, Types ...>::Type;
+    using Type = typename ApplyTypeListForClass<Class, typename List::Tail, typename List::Head, Types ...>::Type;
 };
 
-template <template <typename ...> typename Class, template <> typename List, typename ... Types>
-struct ApplyTypeListForClass<Class, List, Types ...>
+template <template <typename ...> typename Class, typename ... Types>
+struct ApplyTypeListForClass<Class, TypeList<>, Types ...>
 {
     using Type = typename Class<Types ...>;
 };
