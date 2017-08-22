1a51f8a00ade441e579fedb8695e06849f85ec23
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2017-08-22 13:51:51 +0300

    updated GatherUtils

--- dbms/src/Functions/GatherUtils.h
+++ dbms/src/Functions/GatherUtils.h
@@ -1078,7 +1078,7 @@ struct ArraySourceSelector<Base, Arg, Type, Types ...>
     static void select(IArraySource & source, Arg & arg)
     {
         if (auto array = typeid_cast<NumericArraySource<Type> *>(&source))
-            Base<Arg, Types ...>::selectImpl<NumericArraySource<Type>>(*array, arg);
+            Base<Arg, Types ...>::template selectImpl(*array, arg);
         else if(auto nullable_array = typeid_cast<NullableArraySource<NumericArraySource<Type>> *>(&source))
             Base<Arg, Types ...>::selectImpl(*nullable_array, arg);
         else if (auto const_array = typeid_cast<ConstSource<NumericArraySource<Type>> *>(&source))
